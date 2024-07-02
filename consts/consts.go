47c0b0a9f7ab7d63be171b227d39af58e0d27970
phoebe-267 <nwabunormary@gmail.com>
2024-07-02 13:42:41 +0000

    phoebe

--- consts/consts.go
+++ consts/consts.go
@@ -13,11 +13,11 @@ import (
 
 const (
 	// TODO: choose a human-readable part for your hyperchain
-	HRP = ""
+	HRP = "phoebe"
 	// TODO: choose a name for your hyperchain
-	Name = ""
+	Name = "phibi"
 	// TODO: choose a token symbol
-	Symbol = ""
+	Symbol = "phb"
 )
 
 var ID ids.ID
